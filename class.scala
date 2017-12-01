d451f6b7feef1b664a91ba75068ed47fb8b1a9d1
linuxandchill <rivercodi@gmail.com>
2017-12-01 10:03:50 -0800

    class

---
+++ class.scala
@@ -0,0 +1,5 @@
+class Person(val name: String, var age: Int){
+  println(s"I am $name, I am $age years old")
+}
+
+var john = new Person("John", 40)
---
+++ practice.scala
@@ -0,0 +1,3 @@
+class Point(var x: Int = 0, var y: Int = 0)
+val point2 = new Point(y=2)
+println(point2.y)  // prints 2
